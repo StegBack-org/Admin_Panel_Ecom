cfab040caa64c95a2457b8e62b4a1c421f579352
Kartik Mandothiya <kartikya27@gmail.com>
2023-10-07 01:58:47 +0530

    setup package

--- package/PanelPulse/src/routes/web.php
+++ package/PanelPulse/src/routes/web.php
@@ -1,1 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('admin', function () {
+    return "Hello";
+});
---
+++ package/PanelPulse/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kartikey/panel-pulse',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ec63dcec015ff38f332dbb6c51da73559d0eb616',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kartikey/panel-pulse' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ec63dcec015ff38f332dbb6c51da73559d0eb616',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
