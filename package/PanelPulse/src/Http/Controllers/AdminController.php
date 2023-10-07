384dae86f707c82ebd03f5cf9bb273c3c92eb21b
Kartik Mandothiya <kartikya27@gmail.com>
2023-10-07 10:00:13 +0530

    test

--- package/PanelPulse/src/Http/Controllers/AdminController.php
+++ package/PanelPulse/src/Http/Controllers/AdminController.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function index()
     {
--- package/PanelPulse/src/routes/web.php
+++ package/PanelPulse/src/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Kartikey\PanelPulse\Http\Controllers\AdminController;
 
+Route::get('admin', [AdminController::class, 'index']);
 
-Route::get('admin', function () {
-    return view('PanelPulse::admin.home');
-});
+// Route::get('admin', function () {
+//     return view('PanelPulse::admin.home');
+// });
 
-Route::get('/login', [AuthController::class, 'login'])->name('admin-login');
+// Route::get('/login', [AuthController::class, 'login'])->name('admin-login');
+Route::get('login', function () {
+    return "Login Page";
+})->name('admin-login');
